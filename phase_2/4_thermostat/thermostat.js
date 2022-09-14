8622bd0a8d87e83e2b6c3942811083a7ebb70e41
MarcoDaz <80964100+MarcoDaz@users.noreply.github.com>
2022-09-14 13:48:27 +0100

    completed thermostat

---
+++ phase_2/4_thermostat/thermostat.js
@@ -0,0 +1,31 @@
+class Thermostat {
+  constructor() {
+    this.temperature = 20;
+    this.minTemperature = 10;
+    this.maxTemp = 25;
+  }
+
+  getTemperature() {
+    return this.temperature;
+  }
+
+  up() {
+    if (this.temperature < this.maxTemp) {
+      this.temperature++;
+    };
+  }
+
+  down() {
+    if (this.temperature > this.minTemperature) {
+      this.temperature--;
+    };
+  }
+
+  setPowerSavingMode(bool) {
+    this.maxTemp = bool ? 25 : 32;
+    this.temperature = this.temperature > this.maxTemp ?
+      this.maxTemp : this.temperature;
+  }
+};
+
+module.exports = Thermostat;
